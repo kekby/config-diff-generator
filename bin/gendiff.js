7465ccff05b9add87b5a4301ba8cf63817a3491d
Bulat Gombotsyrenov <gombotsyrenov_bb@drom.ru>
2021-03-26 12:23:17 +0700

    move commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import commander from 'commander';
 import genDiff from '../src/index.js';
+
+const { Command } = commander;
 
 const program = new Command();
 
